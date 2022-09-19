0fbc8e36a3f9f8b6535a41236cb476a56de1b361
htqanh305 <quynhanhhuynh305@gmail.com>
2022-09-19 14:53:28 -0500

    Card functionaility finished

--- frontend/src/components/VocabItem.jsx
+++ frontend/src/components/VocabItem.jsx
@@ -10,14 +10,28 @@ function VocabItem({vocab}) {
 
     return (
     <div className="vocab">
-        <div>
-            {new Date(vocab.createdAt).toLocaleDateString('en-US')}
+                
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <div className="card-block">
+                        <div>
+                            {new Date(vocab.createdAt).toLocaleDateString('en-US')}
+                        </div>
+                        <h2>{vocab.word}</h2>
+                            
+                        <h3>My sentence: "{vocab.sentence}"</h3>
+                    </div>
+                </div>
+                <div class="flip-card-back">
+                    <div className="card-block">
+                        <h3>Definition: {vocab.definition}</h3>
+                    </div>
+                </div>
+            </div>
         </div>
-        <h2>{vocab.word}</h2>
-        <h3>Definition: {vocab.definition}</h3>
-        <h3>My sentence: "{vocab.sentence}"</h3>
 
-
+        
        <button onClick={() => setButtonPopup(true)} className="edit" > Edit </button>
        {/*<button onClick={async () => {
            await dispatch(editVocab(vocab._id))
